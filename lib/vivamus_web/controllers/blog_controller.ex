c5d91c60893a0299164f8d85ee71b780b2020710
Oskar Legner <oskarlegner@gmail.com>
2023-10-22 22:02:08 +0200

    feat(posts): 🤡 Mock posts

--- lib/vivamus_web/controllers/blog_controller.ex
+++ lib/vivamus_web/controllers/blog_controller.ex
@@ -1,7 +1,31 @@
 defmodule VivamusWeb.BlogController do
   use VivamusWeb, :controller
 
+  @test_data [
+    %{
+      user: %{
+        username: "Test user",
+        avatar_url: "https://cataas.com/cat/says/1"
+      },
+      content: "Test content"
+    },
+    %{
+      user: %{
+        username: "Other user",
+        avatar_url: "https://cataas.com/cat/says/2"
+      },
+      content: "Test content 2"
+    },
+    %{
+      user: %{
+        username: "Test user",
+        avatar_url: "https://cataas.com/cat/says/3"
+      },
+      content: "Test content 3"
+    }
+  ]
+
   def home(conn, _params) do
-    render(conn, :home)
+    render(conn, :home, posts: @test_data)
   end
 end
